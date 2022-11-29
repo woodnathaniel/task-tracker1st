21b4ce19f61ed2cf27139ebda2e2cb6658b56008
woodnathaniel <82489020+woodnathaniel@users.noreply.github.com>
2022-11-29 13:58:25 +0000

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,15 +1,11 @@
 import Button from './Button';
 
-
 const Header = (props) =>{
-    
     return(
         <div className='header'>
             <h1>{props.title}</h1>
             <Button />
         </div>
     )
-
-   
 };export default Header;
 
